6905acf52c65f9f9b223ef23200f8ef014618d5e
Ridam Hazra <rhazra0602@gmail.com>
2021-10-19 13:15:48 +0530

    Create 236A - Boy or Girl.cpp

---
+++ Practice/CodeForces/236A - Boy or Girl.cpp
@@ -0,0 +1,39 @@
+/*
+
+Those days, many boys use beautiful girls' photos as avatars in forums. So it is pretty hard to tell the gender of a user at the first glance. Last year, our hero went to a forum and had a nice chat with a beauty (he thought so). After that they talked very often and eventually they became a couple in the network.
+
+But yesterday, he came to see "her" in the real world and found out "she" is actually a very strong man! Our hero is very sad and he is too tired to love again now. So he came up with a way to recognize users' genders by their user names.
+
+This is his method: if the number of distinct characters in one's user name is odd, then he is a male, otherwise she is a female. You are given the string that denotes the user name, please help our hero to determine the gender of this user by his method.
+
+*/
+
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {    
+    int l=0;
+    string s;
+    cin >> s;
+    
+    set<char> s1;
+    for(char c:s)
+    {
+        s1.insert(c);
+    }
+    
+    l = s1.size();
+    
+    if(l%2==0)
+    {
+        cout << "CHAT WITH HER!" << endl;
+    }
+    else
+    {
+        cout << "IGNORE HIM!" << endl;
+    }
+    
+    return 0;
+}
