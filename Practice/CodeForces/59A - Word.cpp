cdef1a12b1400b1beeb0c4f3b9b86794ef84b313
Ridam Hazra <rhazra0602@gmail.com>
2021-10-20 14:15:38 +0530

    Create 59A - Word.cpp

---
+++ Practice/CodeForces/59A - Word.cpp
@@ -0,0 +1,36 @@
+/*
+
+Vasya is very upset that many people on the Net mix uppercase and lowercase letters in one word. That's why he decided to invent an extension for his favorite browser that would change the letters' register in every word so that it either only consisted of lowercase letters or, vice versa, only of uppercase ones. At that as little as possible letters should be changed in the word. For example, the word HoUse must be replaced with house, and the word ViP — with VIP. If a word contains an equal number of uppercase and lowercase letters, you should replace all the letters with lowercase ones. For example, maTRIx should be replaced by matrix. Your task is to use the given method on one given word.
+
+*/
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+#include <cmath>
+ 
+using namespace std;
+ 
+int main() {
+    int n=0;
+    string str;
+    cin >> str;
+    
+    for(char c: str)
+    {
+        if (islower(c))
+            n++;
+        else
+            n--;
+    }
+    
+    for(char c : str)
+    {
+        if(n>=0)
+            cout << (char)tolower(c);
+        else 
+            cout << (char)toupper(c);
+    }
+}
