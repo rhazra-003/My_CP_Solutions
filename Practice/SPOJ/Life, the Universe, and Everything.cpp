649d3b469810c638ef227f160db3fbe9781deb07
Ridam Hazra <rhazra0602@gmail.com>
2021-11-01 23:35:16 +0530

    Add files via upload

---
+++ Practice/SPOJ/Life, the Universe, and Everything.cpp
@@ -0,0 +1,18 @@
+// https://www.spoj.com/problems/TEST/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	while (true)
+	{
+		cin >> t;
+		if (t == 42)
+			break;
+		else
+			cout << t << endl;
+	}
+	
+	return 0;
+}
