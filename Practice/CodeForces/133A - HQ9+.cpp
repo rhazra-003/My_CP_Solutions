02dcdf1b55d98414009eff76189bd8c7f289ad49
Ridam Hazra <rhazra0602@gmail.com>
2021-11-04 22:31:10 +0530

    Create 133A - HQ9+.cpp

---
+++ Practice/CodeForces/133A - HQ9+.cpp
@@ -0,0 +1,25 @@
+// https://codeforces.com/contest/133/problem/A
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    for(int i=0; i<s.length(); i++)
+    {
+        if(s[i] == 'H' || s[i] == 'Q' || s[i] == '9')
+        {
+            cout << "YES" << endl;
+            return 0;
+        }
+    }
+    
+    cout << "NO" << endl;
+    
+    return 0;
+}
