0f6989d5871eddecdbe517cdea066897af439bed
Ridam Hazra <rhazra0602@gmail.com>
2021-11-04 22:28:53 +0530

    Create 432A - Choosing Teams.cpp

---
+++ Practice/CodeForces/432A - Choosing Teams.cpp
@@ -0,0 +1,32 @@
+// https://codeforces.com/contest/432/problem/A
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main ()
+{
+    int n, m;
+    while (cin >> n >> m)
+    {
+        int ans=0,temp=5-m;
+        
+        while (n--)
+        {
+            int temp2;
+            
+            cin >> temp2;
+            
+            if (temp2<=temp)
+            {
+                ans++;
+            }
+                
+        }
+        
+        cout << ans/3 << endl;
+    }
+    
+    return 0;
+}
