7a88472e90f0522ac47cc9889689f37d70a30de2
Ridam Hazra <rhazra0602@gmail.com>
2021-12-06 22:29:27 +0530

    Create MINDAYSRET.cpp

---
+++ Practice/CodeChef/MINDAYSRET.cpp
@@ -0,0 +1,23 @@
+// https://www.codechef.com/problems/MINDAYSRET
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n, k;
+	    cin >> n >> k;
+	    
+	    if(n%k == 0)
+	    {
+	        cout << n/k << endl;
+	    }
+	    else
+	    {
+	        cout << n/k + 1 << endl;
+	    }
+	}
+	return 0;
+}
