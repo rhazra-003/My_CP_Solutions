5233e3a1a66ecd710ee210ca9b7a9ce279248438
Ridam Hazra <rhazra0602@gmail.com>
2021-12-16 22:44:43 +0530

    Create 466A - Cheap Travel.cpp

---
+++ Practice/CodeForces/466A - Cheap Travel.cpp
@@ -0,0 +1,23 @@
+// https://codeforces.com/contest/466/problem/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, m, a, b;
+    cin >> n >> m >> a >> b;
+    
+    if(m*a <= b)
+    {
+        cout << n*a << endl;
+    }
+    else
+    {
+        cout << (n/m)*b + min((n%m)*a, b) << endl;
+    }
+    
+    return 0;
+    
+}
