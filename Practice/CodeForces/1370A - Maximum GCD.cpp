d17abec44464d1113d4fc6f8f1d56077f1ffb630
Ridam Hazra <rhazra0602@gmail.com>
2021-12-19 22:23:02 +0530

    Create 1370A - Maximum GCD.cpp

---
+++ Practice/CodeForces/1370A - Maximum GCD.cpp
@@ -0,0 +1,21 @@
+// https://codeforces.com/contest/1370/problem/A
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    long long n;
+    
+    cin >> t;
+    
+    while(t--)
+    {
+        cin >> n;
+        cout << n/2 << endl;
+    }
+
+    return 0;
+}
