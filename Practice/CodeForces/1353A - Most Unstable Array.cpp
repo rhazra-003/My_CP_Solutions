bef7706fcaa5333d068b37739ba5c371b49e37d5
Ridam Hazra <rhazra0602@gmail.com>
2022-02-28 15:58:50 +0530

    Create 1353A - Most Unstable Array.cpp

---
+++ Practice/CodeForces/1353A - Most Unstable Array.cpp
@@ -0,0 +1,33 @@
+// https://codeforces.com/contest/1353/problem/A
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    long long int n, m;
+    cin>>t;
+    
+    while(t--)
+    {
+        cin >> n >> m;
+        if(n == 1)
+        {
+            cout << 0 <<endl;
+            continue;
+        }
+        if(n == 2)
+        {
+            cout << m <<endl;
+            continue;
+        }
+        if(n>2)
+        {
+            cout << m*2 << endl;
+        }
+    }
+    
+    return 0;
+}
