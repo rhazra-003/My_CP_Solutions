05f613f405cc10f4f06ef9a4b34693f01536d389
Ridam Hazra <rhazra0602@gmail.com>
2022-03-02 17:13:25 +0530

    Create 935A - Fafa and his Company.cpp

---
+++ Practice/CodeForces/935A - Fafa and his Company.cpp
@@ -0,0 +1,28 @@
+// https://codeforces.com/problemset/problem/935/A
+
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+    int n, cnt=1;
+    cin>>n;
+    
+    for(int i=2; i*i<n; i++)
+    {
+        if(n%i == 0)
+        {
+            cnt += 2;
+        }    
+    }
+    if((double)sqrt(n) == (int)sqrt(n))
+    {
+        cnt++;
+    }
+    
+    cout << cnt << endl;
+    
+    return 0;
+}
