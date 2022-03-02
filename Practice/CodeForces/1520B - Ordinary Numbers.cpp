5fed9780c90512335a0039333801866762c69ecc
Ridam Hazra <rhazra0602@gmail.com>
2022-03-02 15:14:26 +0530

    Create 1520B - Ordinary Numbers.cpp

---
+++ Practice/CodeForces/1520B - Ordinary Numbers.cpp
@@ -0,0 +1,31 @@
+// https://codeforces.com/problemset/problem/1520/B
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    
+    while (t--){
+        long long int n;
+        int ans=0;
+        cin >> n;
+        
+        for (long long i=1; i<=n; i = i * 10 + 1)
+        {
+            for (int j=1; j<=9;  j++)
+            {
+                if (i * j <= n)
+                {
+                    ans++;
+                }
+            }
+        }
+        
+        cout << ans << endl;
+	}
+    
+    return 0;
+}
