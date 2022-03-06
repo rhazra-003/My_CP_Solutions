97d5d0aefb9c80884176b4db94a26a2476e609af
Ridam Hazra <rhazra0602@gmail.com>
2022-03-06 21:50:06 +0530

    Create 219A - k-String.cpp

---
+++ Practice/CodeForces/219A - k-String.cpp
@@ -0,0 +1,48 @@
+// https://codeforces.com/problemset/problem/219/A
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int k, count=0;
+	char ch;
+	string s;
+	cin >> k;
+	cin >> s;
+	int l=s.size();
+	
+	sort(s.begin(), s.begin()+s.size());
+	
+	for(int i=0; i<s.size(); i++)
+	{
+	    if(i%k == 0)
+	    {
+	       ch = s[i]; 
+	    }
+	    
+	    if(s[i] == ch)
+	    {
+	        count++;
+	    }
+	}
+	
+	if(l==count && count%k==0)
+	{
+	    for(int i=0; i<k; i++)
+	    {
+	        for(int l=0; l<s.size(); l+=k)
+	        {
+	            cout << s[l];
+	        }
+	    }
+	}
+	
+	else
+	{
+	    cout << "-1" << endl;
+	}
+
+	return 0;
+}
