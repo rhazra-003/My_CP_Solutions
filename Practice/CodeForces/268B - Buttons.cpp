643d24f291c402fef5ebb00325a4af7ce5a43273
Ridam Hazra <rhazra0602@gmail.com>
2022-03-09 00:00:27 +0530

    Create 268B - Buttons.cpp

---
+++ Practice/CodeForces/268B - Buttons.cpp
@@ -0,0 +1,15 @@
+// https://codeforces.com/problemset/problem/268/B
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+
+    cout << (n - 1) * n * (n + 1) / 6 + n << endl;
+    
+    return 0;
+}
